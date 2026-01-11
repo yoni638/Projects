27f637690c1a1700d1151f915ac91774965aa057
yoni638 <yonatangetachew695@gmail.com>
2026-01-12 00:27:19 +0300

    Update config.php

--- coffee/config.php
+++ coffee/config.php
@@ -1,16 +1,16 @@
 <?php
 return [
-    'DB_HOST' => 'mysql-db01.remote',
-    'DB_PORT' => 31636,
-    'DB_USER' => 'coffee_friend',
-    'DB_PASS' => 'ZdwuHh0&j&5sp0vA',
-    'DB_NAME' => 'coffee_friend_bot',
-    'BOT_TOKEN' => '7826690833:AAHK7RPRBKDQ92Y95jwQ4-HyvGiT1KNkXUE',
+    'DB_HOST' => '',
+    'DB_PORT' => ,
+    'DB_USER' => '',
+    'DB_PASS' => '',
+    'DB_NAME' => '',
+    'BOT_TOKEN' => '',
     'ENCRYPTION_KEY' => 'your-32-character-secret-key!!', // Change this!
-    'ADMIN_USERNAME' => '@yadaelaha',
-    'BANK_ACCOUNT' => '1000474777476',
-	'ADMIN_TELEGRAM_ID' =>'8355811425',
-	'ADMIN_BOT_TOKEN' =>'8516282021:AAGQkSq4iFA4dE3_eTfslpbUlp-l2EUeptc'
+    'ADMIN_USERNAME' => '',
+    'BANK_ACCOUNT' => '',
+	'ADMIN_TELEGRAM_ID' =>'',
+	'ADMIN_BOT_TOKEN' =>''
 	 
 
 ];
